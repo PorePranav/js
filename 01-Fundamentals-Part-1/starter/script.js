b48f5ba207543d3141ad307951c703486a18019f
Pranav Pore <pranav@pranavpore.com>
2022-12-01 23:40:45 +0530

    updated Funda-Part-1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = 'amazing';
+if(js==='amazing')
+    alert('JS is fun!');
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Pranav Pore")
+
+let firstName = "Pranav"
+let age = 19
+
+let PI = 3.1415
+
