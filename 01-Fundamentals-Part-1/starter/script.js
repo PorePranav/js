9e165dcf7c2266352b3e6b9325b793cd063ba756
Pranav Pore <pranav@pranavpore.com>
2022-12-06 18:58:40 +0530

    updated script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,38 @@
-let js = 'amazing';
-if(js==='amazing')
-    alert('JS is fun!');
+// let js = 'amazing';
+// if(js==='amazing')
+//     alert('JS is fun!');
 
 console.log(40 + 8 + 23 - 10);
 
 console.log("Pranav Pore")
 
-let firstName = "Pranav"
-let age = 19
+//let firstName = "Pranav"
+//let age = 19
 
 let PI = 3.1415
 
+//This is a comment
+true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Pranav");
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 19;
+const birthYear = 2003;
