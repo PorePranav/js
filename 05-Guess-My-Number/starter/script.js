c7f01888222e7de4ba8145e8de80bf6d3c349209
Pranav Pore <pranav@pranavpore.com>
2022-12-09 16:17:24 +0530

    updated code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+/*
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
+*/
+
+const number = Math.trunc(Math.random() * 20);
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number entered!";
+  }
+
+  if (guess === number) {
+    document.querySelector(".message").textContent =
+      "Congratulations, you won! 🎉";
+  } else {
+    document.querySelector(".message").textContent = "Try again! 🤔";
+  }
+});
