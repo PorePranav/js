9aa67c45763b11dd022ffa666015cf0711189651
Pranav Pore <pranav@pranavpore.com>
2022-12-16 00:23:06 +0530

    added code

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear){
+    const age = 2022 - birthYear;
+
+    function printAge(){
+        const output = `You are ${age}, born in the year ${birthYear}`;
+        console.log(output);
+
+        if(birthYear >= 1981 && birthYear <= 1996){
+            var millenial = true;
+            console.log(`Oh, and you are a millenial ${firstName}`);
+        }
+
+        console.log(millenial);
+    }
+    printAge();
+
+    return age;
+}
+
+const firstName = "Pranav";
+calcAge(2003);
