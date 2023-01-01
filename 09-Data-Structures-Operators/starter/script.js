4da7c01ad83bc219c573c17aa3245d93f772778b
Pranav Pore <pranav@pranavpore.com>
2023-01-01 22:31:55 +0530

    added code

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,5 +11,48 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, menuIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[menuIndex]];
+  },
   
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, ,secondary] = restaurant.categories;
+console.log(main, secondary);
+
+
+//Swapping elements without destructing
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+//Swapping elements with destructing;
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 3, 4, [5, 6]];
+console.log(nested[3]);
+console.log(nested[3][0]);
+console.log(nested[3][1]);
+
+//Nested destructuring
+const [i, , , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p, q, r = 1] = [8, 9];
+console.log(p, q, r); 
